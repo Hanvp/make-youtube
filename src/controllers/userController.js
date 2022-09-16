53b5fa4dab444077767485076b30ee878282c1ab
Hanvp <nwactris@naver.com>
2022-09-16 15:16:10 +0900

    header fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -185,7 +185,7 @@ export const finishGithubLogin = async (req,res)=>
 
 export const logout = (req,res) => {
     req.session.destroy();
-    req.flash("info", "Bye Bye");
+    //req.flash("info", "Bye Bye");
     return res.redirect("/");
 };
 export const see = async (req,res) => {
